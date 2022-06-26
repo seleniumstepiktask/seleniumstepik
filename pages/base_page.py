df890383a7156353e9fbc9e046a8b63d6fd18579
seleniumstepiktask <kehivas251@tagbert.com>
2022-06-26 15:16:14 +0300

    update project structure, new tests for product page

--- pages/base_page.py
+++ pages/base_page.py
@@ -3,6 +3,7 @@ import math
 from selenium.common import NoSuchElementException, NoAlertPresentException, TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+from .locators import BasePageLocators
 
 
 class BasePage:
@@ -46,6 +47,13 @@ class BasePage:
             alert.accept()
         except NoAlertPresentException:
             print("No second alert presented")
+
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        login_link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
 
     def open(self):
         self.browser.get(self.url)
